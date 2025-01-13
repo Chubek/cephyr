1b6e4787ad026d70ed24c55ce56155ec0b8d3d54
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-01-13 10:49:17 +0330

    Added: Computation of post- and immediate- dominators

--- source/flow.d
+++ source/flow.d
@@ -1,6 +1,6 @@
 module cephyr.cfg;
 
-import std.typecons, std.variant, std.sumtype, std.array, std.algorithm, std.range;
+import std.typecons, std.variant, std.sumtype, std.array, std.algorithm, std.range, std.conv;
 
 import cephyr.set;
 import cephyr.stack;
@@ -11,14 +11,19 @@ alias Daton = Assem;
 
 class BasicBlock
 {
+    enum DJB2_INIT = 5381;
+
     Set!BasicBlock predecessors;
     Set!BasicBlock successors;
     Assem data;
+    int id;
     bool is_terminal;
+    static int id_counter;
 
     this(Assem data, bool is_terminal = false)
     {
         this.data = data;
+        this.id = this.id_counter++;
         this.is_terminal = is_terminal;
     }
 
@@ -31,6 +36,19 @@ class BasicBlock
     {
         this.successors ~= block;
     }
+
+    bool opEquals(const BasicBlock rhs) const
+    {
+        return this.id == rhs.id;
+    }
+
+    size_t toHash()
+    {
+        size_t hash = DJB2_INIT;
+        foreach (chr; this.id.to!string)
+            hash = (hash << 5) + hash + chr;
+        return hash;
+    }
 }
 
 class CFG
@@ -39,6 +57,7 @@ class CFG
     alias BBSet = Set!BasicBlock;
     alias Edges = Set!BBlockEdge;
     alias Dominators = BBSet[BasicBlock];
+    alias IDoms = BasicBlock[BasicBlock];
 
     BasicBlock entry;
     BasicBlock exit;
@@ -83,20 +102,122 @@ class CFG
                 if (node == this.entry)
                     continue;
 
-                BBSet intersection = BBSet();
+                BBSet intersection = this.nodes.dup;
                 foreach (pred; node.predecessors[])
-                    intersection = intersection & pred;
+                    intersection = intersection & output[pred];
 
-                auto new_output = output.dup;
-                new_output[node] = Set(node) + intersection;
+                intersection ~= node;
 
-                if (output != new_output)
+                if (output[node] != intersection)
                 {
-                    output = new_output.dup;
+                    output[node] = intersection;
                     changed = true;
                 }
             }
         }
 
+    }
+
+    Dominators computePostDominators()
+    {
+        Dominators output = null;
+        output[this.entry] = Set(this.entry);
+
+        foreach (node; this.nodes[])
+        {
+            if (node == this.entry)
+                continue;
+            output[node] = this.nodes;
+        }
+
+        bool changed = true;
+        while (changed)
+        {
+            changed = false;
+
+            foreach (node; this.nodes[])
+            {
+                if (node == this.entry)
+                    continue;
+
+                BBSet intersection = this.nodes.dup;
+                foreach (succ; node.successors[])
+                    intersection = intersection & output[succ];
+
+                intersection ~= node;
+
+                if (output[node] != intersection)
+                {
+                    output[node] = intersection;
+                    changed = true;
+                }
+            }
+        }
+
+    }
+
+    Dominators computeDominanceFrontiers(Dominators dominators)
+    {
+        Dominators output = null;
+        foreach (node; this.nodes[])
+            output[node] = BBSet();
+
+        foreach (node; this.nodes[])
+        {
+            foreach (succ; node.successors[])
+            {
+                auto runner = Set(succ);
+                while (runner != dominators[node])
+                {
+                    output[runner] ~= node;
+                    runner = dominators[runner];
+                }
+            }
+        }
+
+        return output;
+    }
+
+    Dominators computePostDominanceFrontiers(Dominators post_dominators)
+    {
+        Dominators output = null;
+        foreach (node; this.nodes[])
+            output[node] = BBSet();
+
+        foreach (node; this.nodes[])
+        {
+            foreach (pred; node.predecessors[])
+            {
+                auto runner = Set(pred);
+                while (runner != post_dominators[node])
+                {
+                    output[runner] ~= node;
+                    runner = post_dominators[runner];
+                }
+            }
+        }
+
+        return output;
+    }
+
+    IDoms computeImmediateDominators(Dominators dominators)
+    {
+        IDoms idoms = null;
+
+        foreach (node; this.nodes[])
+        {
+            BasicBlock idom = null;
+            foreach (dom; dominators[node][])
+            {
+                if (dom != node)
+                {
+                    if (!idom || dominators[dom].getLength() > dominators[idom].getLength())
+                        idom = dom;
+                }
+                idoms[dom] = idom;
+            }
+        }
+
+        return idoms;
     }
 }
