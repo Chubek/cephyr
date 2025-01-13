59a6246fc92022c17cfcd41e99f663287c297691
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-01-13 13:33:36 +0330

    Revised: The `Temporary` structure was revised --- making it much simpler

--- source/temporary.d
+++ source/temporary.d
@@ -4,143 +4,43 @@ import std.typecons, std.variant, std.sumtype, std.array, std.algorithm, std.con
 
 struct Temporary
 {
-    alias TempId = long;
-    alias Color = int;
-
-    enum Size
-    {
-        Byte,
-        Half,
-        Double,
-        Quad,
-    }
-
     enum Kind
     {
-        Spilled,
+        InFrame,
         InRegister,
-        SavedRegister,
+        InBinary,
     }
 
-    Size size;
-    Kind kind;
-    TempId id;
-    string value;
     size_t offset;
-    bool defines;
-    Color color = -1;
-    static TempId id_counter;
+    size_t size;
+    static size_t offset_counter;
 
-    this(Kind kind)
-    {
-        this.kind = kind;
-        this.id = id_counter++;
-    }
-
-    this(Kind kind, string value)
-    {
-        this.kind = kind;
-        this.value = value;
-        this.id = id_counter++;
-    }
-
-    this(Kind kind, Size size, size_t offset)
+    this(Kind kind, size_t size)
     {
         this.kind = kind;
         this.size = size;
-        this.offset = offset;
-        this.id = id_counter++;
+        this.offset = this.offset_counter;
+        this.offset_counter += size;
     }
 
-    this(Kind kind, size_t offset)
+    void resetOffsetCounter()
     {
-        this.kind = kind;
-        this.offset = offset;
-        this.id = id_counter++;
+        this.offset_counter = 0;
     }
 
-    void setDefines()
+    static Temporary newInFrame(size_t size)
     {
-	this.defines = true;
+        return Temporary(Kind.InFrame, size);
     }
 
-    void setUses()
+    static Temporary newInRegister(size_t size)
     {
-	this.defines = false;
+        return Temporary(Kind.InRegister, size);
     }
 
-    void resetIdCounter()
+    static Temporary newInBinary(size_t size)
     {
-        this.id_counter = 0;
-    }
-
-    TempId getId() const
-    {
-        return this.id;
-    }
-
-    void setColor(Color color)
-    {
-        this.color = color;
-    }
-
-    Color getColor() const
-    {
-        return this.color;
-    }
-
-    void spillToMemory(size_t offset, Size size)
-    {
-        this.kind = Kind.Spilled;
-        this.offset = offset;
-        this.size = size;
-    }
-
-    static Temporary newSpilled(size_t offset)
-    {
-        return Temporary(Kind.Spilled, offset);
-    }
-
-    static Temporary newSpilledByte(size_t offset)
-    {
-        return Temporary(Kind.Spilled, Size.Byte, offset);
-    }
-
-    static Temporary newSpilledHalf(size_t offset)
-    {
-        return Temporary(Kind.Spilled, Size.Half, offset);
-    }
-
-    static Temporary newSpilledDouble(size_t offset)
-    {
-        return Temporary(Kind.Spilled, Size.Double, offset);
-    }
-
-    static Temporary newSpilledQuad(size_t offset)
-    {
-        return Temporary(Kind.Spilled, Size.Quad, offset);
-    }
-
-    static Temporary newInRegister()
-    {
-        return Temporary(Kind.InRegister);
-    }
-
-    static Temporary newSavedRegister(string value)
-    {
-        return Temporary(Kind.SavedRegister, value);
-    }
-
-    string toString() const
-    {
-        import std.format : format;
-
-        if (this.kind == Kind.Spilled)
-            return format("m%d", this.id);
-        else if (this.kind == Kind.InRegister)
-            return format("r%d", this.id);
-        else
-            return format("[%d]{%s}", this.id, this.value);
+        return Temporary(Kind.InBinary, size);
     }
 }
 
@@ -186,18 +86,6 @@ struct Label
     static Label newMangled()
     {
         return Label(Kind.Mangled, generateRandomString());
-    }
-
-    string toString() const
-    {
-        import std.format : format;
-
-        if (this.kind == Kind.Local)
-            return format("%s:", this.name);
-        else if (this.kind == Kind.Global)
-            return format("%s:", this.name);
-        else
-            return format(".%s:", this.name);
     }
 
 }
